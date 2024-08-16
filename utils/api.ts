0c165b54c3b758326e10676e9c339e9beb379006
buchta@brandoo.cz <buchta@brandoo.cz>
2024-08-17 01:03:22 +0200

    backend url

--- utils/api.ts
+++ utils/api.ts
@@ -10,8 +10,8 @@ import {
 } from "lodash";
 
 dotenv.config();
-// export const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL; // LOCAL URL?
-export const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL_PROD; // PROD URL
+export const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL; // LOCAL URL?
+// export const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL_PROD; // PROD URL
 // export const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL_DEV; // DEV URL
 
 const instance: AxiosInstance = axios.create({
